345cbc7e55352d50194ad3f95ab7bec2d204212e
Zaitsev Nick <Mikalai.Zaitsau1@ibm.com>
2021-09-22 06:06:35 +0300

    update

--- client/src/components/Common/Preloader.jsx
+++ client/src/components/Common/Preloader.jsx
@@ -3,18 +3,17 @@ import './Preloader.scss';
 const Preloader = () => {
   return (
     <>
-        <div className='preloader'>
-            <div id='cube'>
-                <div className='front'></div>
-                <div className='back'></div>
-                <div className='right'></div>
-                <div className='left'></div>
-                <div className='top'></div>
-                <div className='bottom'></div>
-            </div>
-            <div id='shadow'>"</div>
+      <div className="preloader">
+        <div id="cube">
+          <div className="front"></div>
+          <div className="back"></div>
+          <div className="right"></div>
+          <div className="left"></div>
+          <div className="top"></div>
+          <div className="bottom"></div>
         </div>
-
+        <div id="shadow"></div>
+      </div>
     </>
   );
 };
